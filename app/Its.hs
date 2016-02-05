1f461f78d3a5b7c744d1db1ffaa621cbe21f38a5
mschaper <michael.schaper@uibk.ac.at>
2016-02-05 11:25:38 +0100

    use tttac format as default output
    we want to output multivariate certificate; but not change the termcomp
    output

--- app/Its.hs
+++ app/Its.hs
@@ -1,10 +1,14 @@
 module Main (main) where
 
 import Tct.Core
+import Tct.Core.Common.Pretty  (pretty, putPretty)
+import Tct.Core.Data.Answer    (tttac)
+import Tct.Core.Data.ProofTree (certificate)
 import Tct.Its
 
 instance Declared Its Its where decls = itsDeclarations
 
 main :: IO ()
-main = runIts itsConfig
+main = runIts $ itsConfig
+  { putAnswer = putPretty . pretty . tttac . certificate }
 
