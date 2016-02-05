f8f6ef88e0141b2b145f809678c2c237dd7ef3fa
mschaper <michael.schaper@uibk.ac.at>
2016-02-05 11:08:58 +0100

    fix: paths in install script

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 mkdir modules &&
-git clone https://github.com/ComputationWithBoundedResources/slogic modules/slogic &&
-git clone https://github.com/ComputationWithBoundedResources/term-rewriting-xml modules/term-rewriting-xml &&
-git clone https://github.com/ComputationWithBoundedResources/tct-core modules/tct-core && pushd tct-core && git checkout cage && popd &&
-git clone https://github.com/ComputationWithBoundedResources/tct-common modules/tct-common &&
-git clone https://github.com/ComputationWithBoundedResources/tct-its modules/tct-its && pushd tct-core && git checkout cage && popd &&
-git clone https://github.com/ComputationWithBoundedResources/tct-trs modules/tct-trs &&
-git clone https://github.com/ComputationWithBoundedResources/tct-inttrs modules/tct-inttrs
+pushd modules &&
+git clone https://github.com/ComputationWithBoundedResources/slogic slogic &&
+git clone https://github.com/ComputationWithBoundedResources/term-rewriting-xml term-rewriting-xml &&
+git clone https://github.com/ComputationWithBoundedResources/tct-core tct-core &&
+pushd tct-core && git checkout cage && popd &&
+git clone https://github.com/ComputationWithBoundedResources/tct-common tct-common &&
+git clone https://github.com/ComputationWithBoundedResources/tct-its tct-its &&
+pushd tct-core && git checkout cage && popd &&
+git clone https://github.com/ComputationWithBoundedResources/tct-trs tct-trs &&
+git clone https://github.com/ComputationWithBoundedResources/tct-inttrs tct-inttrs &&
+popd
